328e970c2c697124fe4375c8f862332c5c9b66ac
Michael Nguyen <michadn2@uci.edu>
2021-02-19 16:07:02 -0800

    adding some logic to exercises

--- src/app/pages/exercises/exercises.page.ts
+++ src/app/pages/exercises/exercises.page.ts
@@ -8,16 +8,54 @@ import * as data from 'Exercises Metadata/Metadata.json';
 })
 
 export class ExercisesPage implements OnInit {
-  public exercises = data.default;
-  constructor() {
+  private exercises;
+  private searchbar;
+  private filterExercises;
 
+
+  constructor() {
+    this.exercises = data.default;
+    this.filterExercises = new Map<any, any>();
+    for (var key in this.exercises){
+      this.filterExercises.set(key, this.exercises[key]);
+    }
+  }
+
+  filteredExercises(){
+    return this.filterExercises;
+  }
+
+  handleInput(event){
+    let query: string = event.target.value.toLowerCase();
+    console.log(query);
+    for (var key in this.exercises){
+      console.log(key.toLowerCase());
+    }
+
+    console.log(this.filterExercises.size);
+
+    if (query){
+      var map = new Map<any, any>();
+      for (var key in this.exercises){
+        console.log(key.toLowerCase());
+        if (key.toLowerCase().startsWith(query)){
+          console.log(query);
+          map.set(key, this.exercises[key]);
+        }
+      }
+
+      // this.filterExercises = map;
+    }
+
+    // console.log(this.filterExercises.size);
   }
 
   ngOnInit() {
+    this.searchbar = document.querySelector('ion-searchbar');
+    this.searchbar.addEventListener('ionInput', this.handleInput);
   }
 
   test(){
     console.log("a");
   }
-
 }
